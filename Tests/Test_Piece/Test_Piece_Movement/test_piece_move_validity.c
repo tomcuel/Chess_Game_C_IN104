d2aa457a90d5bbab13658d509f239e0ec49f6272
Tom Cuel <tom.cuel3@gmail.com>
2024-04-24 18:29:07 +0200

    adding again the Tests directories

---
+++ Tests/Test_Piece/Test_Piece_Movement/test_piece_move_validity.c
@@ -0,0 +1,18 @@
+#include "Constants.h"
+#include "Piece.h"
+#include "Move.h"
+#include "Move_Log.h"
+#include "Board.h"
+#include "Player.h"
+
+
+int main () {
+
+
+
+
+
+
+
+    return EXIT_SUCCESS;
+}
